6c036cd43121d6fdcfe31b7cc53b57b5d25e6a27
GreNaz <nazargrehov@gmail.com>
2023-03-21 15:55:33 +0300

    Refactor 1

--- stat/stat-service/src/main/java/ru/practucum/ewm/controller/StatController.java
+++ stat/stat-service/src/main/java/ru/practucum/ewm/controller/StatController.java
@@ -2,14 +2,9 @@ package ru.practucum.ewm.controller;
 
 import dto.EndpointHitDTO;
 import dto.ViewStatsDTO;
-import io.micrometer.core.lang.Nullable;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.practucum.ewm.service.StatService;
 
 import java.time.LocalDateTime;
@@ -17,7 +12,7 @@ import java.util.List;
 
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class StatController {
     private final StatService service;
